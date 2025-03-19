ff70d2abd72a99a1986a1ce9c9b8f04bfbb11801
risya7 <risyaferlina37@gmail.com>
2025-03-19 07:54:42 +0700

    Add files via upload

--- Tugas2_Risya ferlina_2410432034.py
+++ Tugas2_Risya ferlina_2410432034.py
@@ -12,9 +12,6 @@ delta_x = (b-a) / n
 RP = 0                 # RP adalah luas daerah pada riemann 
 for i in range (n):
     x_i_bar= a + (i+1) * delta_x
-    if x_i_bar >= 0:
-        f_xi_bar = x_i_bar**2 + 2*x_i_bar
-    else :
-        f_xi_bar = 0
+    f_xi_bar = x_i_bar**2 + 2*x_i_bar
     RP = RP + f_xi_bar * delta_x
 print(f"luas daerah dari fungsi x^2 +2x dengan batas daerah x=1 dan x=3 dan jumlah partisi n ({n}) adalah {RP}:")
